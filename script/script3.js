c2ca06c165eb0f2546ccfe8512a47f14bbb2f149
RebeccaTorvik <37801832+RebeccaTorvik@users.noreply.github.com>
2020-06-16 16:50:34 -0400

    Add files via upload

---
+++ script/script3.js
@@ -0,0 +1,27 @@
+
+(function(){
+	$(".flex-slide").each(function(){
+		$(this).hover(function(){
+			$(this).find('.flex-title').css({
+				transform: 'rotate(0deg)',
+				top: '10%'
+			});
+			$(this).find('.flex-about').css({
+				opacity: '1'
+			});
+		}, function(){
+			$(this).find('.flex-title').css({
+				transform: 'rotate(90deg)',
+				top: '15%'
+			});
+			$(this).find('.flex-about').css({
+				opacity: '0'
+			});
+		})
+	});
+	})();
+
+$(window).scroll(function() {
+	var theta = $(window).scrollTop() / 2700 % Math.PI;
+	$('#leftgear').css({ transform: 'rotate(' + theta + 'rad)' });
+	});
